f5ba7c049623627fdac765ccc599e21284cb8d39
Macroinformatica <macroinformaticaedu@gmail.com>
2024-04-25 11:40:25 -0500

    avance implementation

---
+++ settings.gradle.kts
@@ -0,0 +1,23 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "EvangelistaSifuentes_Tarea_03_Ejercicio_01"
+include(":app")
